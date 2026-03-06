bd0509ca2c18efbb745e063da189e49b35a69354
Peter Yu <49627468+Peterodox@users.noreply.github.com>
2026-03-06 16:52:18 +0800

    Added A New GossipID to Auto Show UI
    - Clicking this gossip option shows UIParent immediately (instead of waiting for server to response) to avoid taint error.

--- Code/GossipData/GossipData_ShowUI.lua
+++ Code/GossipData/GossipData_ShowUI.lua
@@ -18,6 +18,7 @@ local ShowUIGossip = {
     [124311] = true,    --(Lorewalking) What stories can you tell me?
     [131481] = true,    --Overcharged Titan Console, <View overcharged console discs.>
     [132979] = true,    --I want to empower my Reshii Wraps.
+    [134809] = true,    --(Quest) Saddle me up! Ranger Valsarin [Quest: 91347] Strider Stampede (Avoid taint)
 };
 
 function GossipDataProvider:DoesOptionOpenUI(gossipOptionID)
